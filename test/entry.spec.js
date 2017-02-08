05aa9265885f70ebcb57e20543c6f04264924b9e
Seve <sdkim@thoughtworks.com>
2017-02-08 14:18:17 -0600

    Trying to test only 1 spec

--- test/entry.spec.js
+++ test/entry.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
-import Entry from '../src/entry.js';
+import Entry from '../src/entry';
 
 describe('Entry', () => {
 
