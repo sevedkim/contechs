e3f2347568fc9ccd917286ce7fddcdef2a91a783
Seve <sdkim@thoughtworks.com>
2017-02-08 12:36:34 -0600

    Changed spec paths

--- test/entryForm.spec.js
+++ test/entryForm.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
-import EntryForm from 'src/entryForm';
+import EntryForm from '../src/entryForm';
 
 describe('Entry Form', () => {
 
