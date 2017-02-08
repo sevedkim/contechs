70b56b13b9c4d2c5eb2e0ab1077dab54e63a79be
Seve <sdkim@thoughtworks.com>
2017-02-08 11:48:12 -0600

    Added travis.yml file

--- src/EntryList.js
+++ src/EntryList.js
@@ -1,27 +1,12 @@
 //entryList.js
 import React from 'react';
 
-// const entryList = (props) => {
-//   const {entries} = props;
-//   console.log('inside entryList');
-//   console.log(entries)
-//   return (
-//     <div>
-//       {entries.map((entry) =>
-//         <div>
-//           {this.entry.term} : {this.entry.definition}
-//         </div>
-//       )}
-//     </div>
-//   )
-// }
-
 const EntryList = ({entries}) => (
   <div>
     List of Entries:
     {entries.map((entry, index) =>
       <div key={index}>
-        { entry.term } : { entry.definition }
+        <b>{ entry.term }</b> : { entry.definition }
       </div>
     )}
   </div>
